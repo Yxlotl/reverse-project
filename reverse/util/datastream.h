37d6cec0ea55e541f1db562e3ee36bdd759d4193
yxlotl <37313266+Yxlotl@users.noreply.github.com>
2023-05-13 02:39:14 -0500

    Delete reverse directory

--- reverse/util/datastream.h
+++
@@ -1,7 +0,0 @@
-#ifndef DATASTREAM
-#define DATASTREAM
-#import <stdio.h>
-
-extern void onEach(FILE* readFrom, FILE* sendTo, (void (*forEach(void* a));
-extern FILE* openFile(const char* path, const char* mode, (void (*errorHandler())));
-#endif
